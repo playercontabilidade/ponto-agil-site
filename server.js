a6451bd17e7d5f5b11a23c1ffb0bd0d3f7e4dc5c
llinhares <luanateixeiralinhares@gmail.com>
2026-08-11 17:31:25 -0300

    rollback: retornar para a versão 7ec772a

--- server.js
+++
@@ -1,6 +0,0 @@
-const app = require('./src/app');
-const config = require('./src/config/config');
-
-app.listen(config.porta, () => {
-  console.log(`Servidor em http://localhost:${config.porta}`);
-});
